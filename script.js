fa83cf198599cdd09eac1adea6e2c3d5395d85c6
jromerob10  <jromerob10@ucentral.edu.co>
2025-10-07 23:53:47 -0500

    Proyecto inicial

---
+++ script.js
@@ -0,0 +1,8 @@
+const accordions = document.querySelectorAll('.accordion-btn');
+
+accordions.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const content = btn.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  });
+});
